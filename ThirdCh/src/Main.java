e9c9c0b3b4e465f121bca1bdfbd6c1f1f15269dc
Zaki-Fadlan <zakifadlan3300@gmail.com>
2022-12-22 00:42:34 +0700

    _

---
+++ ThirdCh/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Data Types".toUpperCase());
+        System.out.println("when declaring variable java must declare specific data type\n" +
+                "Ex: int myNum = 5; etc.\n java data type declared with two group is it Primitive and Non-Primitive" +
+                "\nPrimitive Data type : byte, short, int, long, float, double, boolean, and char" +
+                "\nNon - Primitive data type : String, Arrays, Classes");
+        System.out.println("Primitive Data Types");
+        System.out.println("byte,1 byte, Stores whole number from -128 to 127");
+        System.out.println("short, 2 bytes, Stores whole numbers from -32,768 to 32,767" +
+                "\nint, 4 bytes, Stores whole numbers from -2,147,483,648 to 2,147,483,647" +
+                "\nlong, 8 bytes, Stores whole number from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807" +
+                "\nfloat, 4 bytes, Stores fractional numbers, sufficient for storing 6 to 7 decimal digits" +
+                "\ndouble, 8 bytes, stores fractional numbers. sufficient for storing 15 decimal digits" +
+                "\nboolean, 1 bit, Stores true or false values" +
+                "\nchar,2 bytes, Stores a single charachter/letter or ASCII values");
+
+        System.out.println("Exercise:\n" +
+                "int myNum = 9;\n" +
+                "float myFloatNum = 8.99f;\n" +
+                "char myLetter = 'A';\n" +
+                "boolean myBool = false;\n" +
+                "String myText = \"Hello World\";\n");
+    }
+}
